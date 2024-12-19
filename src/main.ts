2997428040c41d9e091078fd621440f169f88c35
Aldairm0308 <al222111405@gmail.com>
2024-12-18 18:09:03 -0600

    enable cors added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: '*', // Permite todas las URLs, puedes especificar un dominio en vez de '*' si lo prefieres.
+    methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Cabeceras permitidas
+  });
+
+  // Iniciar el servidor
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
